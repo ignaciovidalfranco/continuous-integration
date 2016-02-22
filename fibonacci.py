a5c41fc5f520a8c070c4d8d232799ed513f84c52
nacho <nacho.vfranco@gmail.com>
2016-02-22 17:11:02 +0000

    Added robustness to the fibonacci function. Added more tests

--- fibonacci.py
+++ fibonacci.py
@@ -1,6 +1,10 @@
 def Fibonacci(n):
+	if n < 0:
+			raise ValueError("n < 0 is not valid")
+	elif round(n) != n:
+			raise ValueError("n is not an integer number!")
 	if n < 2:
-		return 1
+		return n
 	else:
 		return Fibonacci(n-1)+Fibonacci(n-2)
 	
--- test_fibonacci.py
+++ test_fibonacci.py
@@ -1,11 +1,21 @@
-def test_fibonacci():
-	"""
-	We import the fibonacci fibonacci.py
-	and we test the function inside itm Fibonacci(n)
-	against known values
-	"""
+import unittest
+
+class FibTest(unittest.TestCase):
 	
-	import fibonacci as fb;
+
+	def test_fibonacci():
+		"""
+		We import the fibonacci fibonacci.py
+		and we test the function inside itm Fibonacci(n)
+		against known values
+		"""
+		
+		import fibonacci as fb;
+		
+		assert fb.Fibonacci(10) == 55
+		assert fb.Fibonacci(5) == 5
 	
-	assert fb.Fibonacci(10) == 55
-	assert fb.Fibonacci(5) == 5
+	def test_Fib_throws_exception(self):
+		import fibonacci as fb
+		self.assertRaises(ValueError, fb.Fibonacci, -5)
+		self.assertRaises(ValueError, fb.Fibonacci, 1.2)
